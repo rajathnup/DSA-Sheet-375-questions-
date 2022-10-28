295e103039e925b7693da2a8f1e484574aa53d16
rajathnup <rajathkalyanappa07@gmail.com>
2022-10-28 19:28:10 +0530

    Memoized solution

--- Dynamic Programming/Min Cost Climbing Stairs-memoization.cpp
+++ Dynamic Programming/Min Cost Climbing Stairs-memoization.cpp
@@ -4,14 +4,17 @@ class Solution {
 public:
     int solve(vector<int>&dp,vector<int>&cost,int n)
     {
+
+        //step2:-Handle base Cases
         if(n==0)return cost[0];
         if(n==1)return cost[1];
 
+        //step4:- memoize the solution
         if(dp[n]!=-1)
         {
             return dp[n];
         }
-
+        //step3:-return the dp[n] if the dp[n]==-1
         dp[n]=cost[n]+min(solve(dp,cost,n-1),solve(dp,cost,n-2));
         return dp[n];
     }
@@ -19,6 +22,8 @@ public:
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
+
+        //step1:-create and intialise the dp array
         vector<int>dp(n+1,-1);
         int ans=min(solve(dp,cost,n-1),solve(dp,cost,n-2));
         return ans;
